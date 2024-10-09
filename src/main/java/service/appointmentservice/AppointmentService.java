dfaeaff62ca4032b3a04b33e51fbddfd6ed7946b
Michael L <mlorenz1012@aol.com>
2024-10-09 13:45:51 -0400

    Fix comments

--- src/main/java/service/appointmentservice/AppointmentService.java
+++ src/main/java/service/appointmentservice/AppointmentService.java
@@ -8,13 +8,12 @@
  * Description:
  * This class supports storing appointments. Allows functionalities to:
  * - add appointments with a unique ID, date, and description
- * - delete appointments per task ID
+ * - delete appointments per appointment ID
  *
  * Date: Due 10/06/2024
  *****************************************************************************/
 package service.appointmentservice;
 
-import service.Service;
 import java.util.Date;
 
 public class AppointmentService extends BasicService<Appointment> {
@@ -34,20 +33,21 @@ public class AppointmentService extends BasicService<Appointment> {
 
     /* UPDATE APPOINTMENT FIELDS ***Disabled for now due to ambiguity in requirements
     /**
-     * Updates name of task with given id to firstName
-     * @param id Unique identifier of the task to update
-     * @param name new task name (non-null, <= 10 chars)
-     * @throws IllegalArgumentException if task does not exist or firstName is invalid
+     * Updates date of appointment with given id to given future date (Date class)
+     * @param id Unique identifier of the appointment to update
+     * @param date new appointment date (non-null, must be after current system time). Chance of exception if date is
+     *             set to actual system time, such as with 'new Date()', use updateDate(String id) instead for setting
+     *             to current time.
+     * @throws IllegalArgumentException if appointment does not exist or date is invalid.
      */
     /*public void updateName(String id, String name) {
         getEntityById(id).updateField("name", name);
     }
 
     /**
-     * Updates name of task with given id to firstName
-     * @param id Unique identifier of the task to update
-     * @param description new task description (non-null, <= 50 chars)
-     * @throws IllegalArgumentException if task does not exist or firstName is invalid
+     * Updates date of appointment with given id to current system time
+     * @param id Unique identifier of the appointment to update
+     * @throws IllegalArgumentException if appointment does not exist or date is invalid
      */
     /*public void updateDescription(String id, String description) {
         getEntityById(id).updateField("description", description);
